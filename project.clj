8cc384271854208984329800e91e93c63b5388f4
Cameron Desautels <camdez@gmail.com>
2015-09-26 21:34:56 -0500

    Bump version number to 0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject camdez/honeybadger "0.1.0"
+(defproject camdez/honeybadger "0.2.0-SNAPSHOT"
   :description "Clojure library for reporting errors to honeybadger.io"
   :url "https://github.com/camdez/honeybadger"
   :license {:name "MIT License"
--- src/honeybadger/core.clj
+++ src/honeybadger/core.clj
@@ -11,7 +11,7 @@
   "Honeybadger for Clojure")
 
 (def notifier-version
-  "0.1.0")
+  "0.2.0-SNAPSHOT")
 
 (def notifier-homepage
   "https://github.com/camdez/honeybadger")
