68c68a5ab57576e88d44608238e705c3c8f736ea
Cameron Desautels <camdez@gmail.com>
2016-04-15 21:28:00 +0800

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/camdez/honeybadger"
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
-  :dependencies [[aleph "0.4.1-beta3"]
+  :dependencies [[aleph "0.4.1"]
                  [clj-stacktrace "0.2.8"]
-                 [prismatic/schema "1.0.4"]
+                 [prismatic/schema "1.1.0"]
                  [org.clojure/clojure "1.7.0"]
                  [org.clojure/data.json "0.2.6"]]
   :profiles {:dev {:injections [(require 'schema.core)
